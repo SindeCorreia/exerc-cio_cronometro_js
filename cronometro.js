8f058cd86e58699ec7532eab017c78df9dfae0ad
Sinde Correia <sindecorreia@gmail.com>
2024-08-15 15:01:26 +0100

    cronometro

--- cronometro.js
+++ cronometro.js
@@ -8,11 +8,14 @@ const resetBtn = document.querySelector("#resetBtn")
 
 let interval;
 let minutes = 0;
-let secoonds = 0 ;
+let seconds = 0 ;
 let milliseconds = 0;
 let isPaused = false;
  
 startBtn.addEventListener("click",startTimer)
+pauseBtn.addEventListener("click",PauserTimer)
+resumeBtn.addEventListener("click",ResumeTimer)
+resetBtn.addEventListener("click",ResetTimer)
 
 function startTimer(){
     interval = setInterval (() => {
@@ -27,8 +30,47 @@ function startTimer(){
                 minutes++;
                 seconds = 0; 
             }
+            minutesEl.textContent = formatTimer(minutes) ;
+            secondsEl.textContent = formatTimer(seconds) ;
+            millisecondsEl.textContent = formatMilliseconds(milliseconds); 
         }
 
-    },10)
-      
+    },10);
+    startBtn.style.display ="none";
+    pauseBtn.style.display="block"    
 }
+function PauserTimer(){
+    isPaused=true;
+    pauseBtn.style.display ="none";
+    resumeBtn.style.display="block"  
+
+}
+function ResetTimer(){
+   clearInterval(interval);
+     minutes = 0;
+     seconds = 0 ;
+     milliseconds = 0;
+
+    minutesEl.textContent = "00" ;
+    secondsEl.textContent = "00" ;
+    millisecondsEl.textContent ="000"; 
+
+    startBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display= "none";  
+
+}
+function ResumeTimer(){
+    isPaused=false;
+    pauseBtn.style.display ="block";
+    resumeBtn.style.display="none"  
+
+}
+
+
+ function formatTimer(time){
+    return time < 10 ? `0${time}` : time ; 
+ }
+ function formatMilliseconds(time){
+    return time < 100 ? `${time}`.padStart(3,"0") : time;
+ }
